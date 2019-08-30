b858ef094f78fa90e851f62c483cd5a07babc318
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-08-30 13:28:21 +0530

    Fusion chart initial installation
    integrate fusion chart initial set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Fusion Charts library
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import './index.css';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// import bootstrap css
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+// column 3d chart configuration settings
+const chartConfigs_colunm2d = {
+  type: 'bar2d',
+  width: '100%',
+  height: 350,
+  dataFormat: 'json',
+  dataSource: {
+    "chart": {
+      "theme": "fusion",
+      "palettecolors": "5d62b5,29c3be,f2726f",
+      "caption": "Lead sources by industry",
+      "subcaption": "2016-2017",
+      "yAxisName": "Number of Leads",
+      "showBorder": "1",
+      "borderColor": "#ccc",
+      "bgColor": "#FFFFFF",
+      //Tooltip customization
+      "toolTipBorderColor": "#666666",
+      "toolTipBgColor": "#efefef",
+      "toolTipBgAlpha": "80",
+      "showToolTipShadow": "1",
+      //Theme
+      "plotBorderThickness": "0.25",
+      "showxaxispercentvalues": "1",
+      "showValues": "1",
+      "plotToolText": "Country: $label <br> Population : $dataValue"
+    },
+    "data": [
+      {
+        label: "Travel & Leisure",
+        value: "41"
+      },
+      {
+        label: "Advertising/Marketing/PR",
+        value: "5"
+      },
+      {
+        label: "Other",
+        value: "15"
+      },
+      {
+        label: "Real Estate",
+        value: "32"
+      }
+    ]
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+          <div className="row">
+            {/*Column 2D section*/}
+            <div className="col-sm-12">
+              <h3>Bar 2D Chart</h3>
+              <ReactFC {...chartConfigs_colunm2d} />
+            </div>
+          </div>
+        </div>)
+  }
 }
 
 export default App;
